5355d33b0bfa7f18b5fd01a1ea40476bfd77c431
ucfilho <42356381+ucfilho@users.noreply.github.com>
2020-07-07 20:36:23 -0300

    Update JDE_Sum_of_different_powers_jul_07_2020.R

--- JDE_Sum_of_different_powers_jul_07_2020.R
+++ JDE_Sum_of_different_powers_jul_07_2020.R
@@ -9,7 +9,7 @@ Sum_of_different_powers= function(x)
 	  {
 	  S=S+abs(x[i])**(i+1) 
 		}
-	fun=1+sumx2/4000 - prod_cosx_i05
+	fun=S
   return(fun)
   }
 # f(x)=0 x=(0,0) [−1,1]
@@ -17,12 +17,12 @@ Sum_of_different_powers= function(x)
 
 dim=10
 RUNS=50
-ITE=1500
+ITE=2000
 Y=0;X=0
 
 for(i in 1:RUNS)
   {
-  JDE_R=JDEoptim(rep(-1, dim), rep(1, dim), Sum_of_different_power,
+  JDE_R=JDEoptim(rep(-1, dim), rep(1, dim), Sum_of_different_powers,
          tol = 1e-20,NP=100, trace = FALSE,  maxiter =ITE)
   Y[i]=JDE_R$value
 
@@ -34,7 +34,7 @@ MEAN=mean(Y)
 STD=sd(Y)
 MAX=max(Y)
 MIN=min(Y)
-cat('Sum_of_different_power JDE DIM=',dim,'RUNS=',RUNS,'ITE=',ITE,'\n')
+cat('Sum_of_different_powers JDE DIM=',dim,'RUNS=',RUNS,'ITE=',ITE,'\n')
 cat('MEAN=',MEAN,'\n')
 cat('MAX',MAX,'\n')
 cat('MIN=',MIN,'\n')
