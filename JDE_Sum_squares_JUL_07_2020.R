a75857406a202513418788e7547fef2d9b25544a
ucfilho <42356381+ucfilho@users.noreply.github.com>
2020-07-07 22:25:25 -0300

    Create JDE_Sum_squares_JUL_07_2020.R

---
+++ JDE_Sum_squares_JUL_07_2020.R
@@ -0,0 +1,35 @@
+library('DEoptimR')
+
+
+Sum_squares= function(x)
+  {
+  Num=length(x)
+  fun=0
+  for (i in 1:Num)
+  {
+    fun = fun+ i *x[i]**2
+   }
+  fun=-fun
+  return(fun)
+  }
+# f(x)=0 x=(0,0) , d=[-10,10]
+
+dim=10
+RUNS=50
+ITE=2000
+Y=0;X=0
+for(i in 1:RUNS)
+  {
+  JDE_R=JDEoptim(rep(-10, dim), rep(10, dim), Sum_squares ,
+         tol = 1e-20,NP=100, trace = FALSE,  maxiter =ITE)
+  Y[i]=JDE_R$value
+  }
+MEAN=mean(Y)
+STD=sd(Y)
+MAX=max(Y)
+MIN=min(Y)
+cat('Sum_squares JDE DIM=',dim,'RUNS=',RUNS,'ITE=',ITE,'\n')
+cat('MEAN=',MEAN,'\n')
+cat('MAX',MAX,'\n')
+cat('MIN=',MIN,'\n')
+cat('STD',STD,'\n')
