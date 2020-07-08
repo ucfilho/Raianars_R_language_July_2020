684a723832112e8f466cfc99d98d10c20ff77f81
ucfilho <42356381+ucfilho@users.noreply.github.com>
2020-07-08 12:50:40 -0300

    Update JDE_Rosenbrock_jul_07_2020.R

--- JDE_Rosenbrock_jul_07_2020.R
+++ JDE_Rosenbrock_jul_07_2020.R
@@ -14,21 +14,23 @@ Rosenbrock= function(x)
   }
 # Global Minimum: 0 , domain=[-30,30] at [1,1,..,1]
 
-dim=10
+dim=30
 RUNS=50
 ITE=2000
+Bounds=30
+NPAR=100
 Y=0;X=0
 for(i in 1:RUNS)
   {
-  JDE_R=JDEoptim(rep(-30, dim), rep(30, dim), Rosenbrock ,
-         tol = 1e-20,NP=100, trace = FALSE,  maxiter =ITE)
+  JDE_R=JDEoptim(rep(-Bounds, dim), rep(Bounds, dim), Rosenbrock ,
+         tol = 1e-20,NP=NPAR, trace = FALSE,  maxiter =ITE)
   Y[i]=JDE_R$value
   }
 MEAN=mean(Y)
 STD=sd(Y)
 MAX=max(Y)
 MIN=min(Y)
-cat('Rosenbrock JDE DIM=',dim,'RUNS=',RUNS,'ITE=',ITE,'\n')
+cat('Rosenbrock JDE DIM=',dim,'RUNS=',RUNS,'ITE=',ITE,'Bounds',-Bounds,Bounds,'\n')
 cat('MEAN=',MEAN,'\n')
 cat('MAX',MAX,'\n')
 cat('MIN=',MIN,'\n')
