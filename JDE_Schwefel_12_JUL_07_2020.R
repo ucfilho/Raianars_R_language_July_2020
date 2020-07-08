82c928dd20800a3b012c0cb5aeef56ff2a9b5502
ucfilho <42356381+ucfilho@users.noreply.github.com>
2020-07-07 23:54:49 -0300

    Create JDE_Schwefel_12_JUL_07_2020.R

---
+++ JDE_Schwefel_12_JUL_07_2020.R
@@ -0,0 +1,36 @@
+library('DEoptimR')
+
+Schwefel_12= function(x)
+  {
+  Num=length(x)
+  sum2=0
+  for (k in 1:Num)
+  {
+        sum1=0
+        for (i in 1:k)  { sum1=sum1+x[i] }
+        sum2=sum2+sum1**2
+   }
+  fun=sum2
+  return(fun)
+  }
+# f(x)=0 x=(0,0) [−100, 100]
+
+dim=10
+RUNS=50
+ITE=2000
+Y=0;X=0
+for(i in 1:RUNS)
+  {
+  JDE_R=JDEoptim(rep(0, dim), rep(pi, dim), Schwefel_12 ,
+         tol = 1e-20,NP=100, trace = FALSE,  maxiter =ITE)
+  Y[i]=JDE_R$value
+  }
+MEAN=mean(Y)
+STD=sd(Y)
+MAX=max(Y)
+MIN=min(Y)
+cat('Schwefel_12 JDE DIM=',dim,'RUNS=',RUNS,'ITE=',ITE,'\n')
+cat('MEAN=',MEAN,'\n')
+cat('MAX',MAX,'\n')
+cat('MIN=',MIN,'\n')
+cat('STD',STD,'\n')
